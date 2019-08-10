76988d6c8fc9c3fce66debed458701ec4abd800d
sr4p <srsai.sr4p@hotmail.com>
2019-08-10 22:23:21 +0700

    Homework week 3

--- app/src/main/java/buu/informatics/s59161126/roller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59161126/roller/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { resetDice() }
+
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = "Dice Rolled!"
     }
@@ -24,7 +27,12 @@ class MainActivity : AppCompatActivity() {
         val randomInt = Random().nextInt(6) + 1
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Random number", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun resetDice(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0"
     }
 
 
